c08eceb577e74e5ef10f2cb8584413074bd2c91c
Matěj Oliva <57315822+Mr-Hyde@users.noreply.github.com>
2020-06-09 11:32:32 +0200

    Update facebook.php

--- include/facebook.php
+++ include/facebook.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'facebook/vendor/autoload.php';
 $fb = new Facebook\Facebook([
-    'app_id' => '553606428650853',
-    'app_secret' => '69b04fa4f0237eb2257bd8d2aa517d52',
+    'app_id' => 'your app id',
+    'app_secret' => 'your secret code',
     'default_graph_version' => 'v4.0'
 ]);
