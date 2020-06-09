3889c7b938436eff1e6908e051f0aa90e217defb
matej oliva <matej.oliva@outlook.com>
2020-06-09 10:33:32 +0200

    [NEW] imported facebook OAuth files

---
+++ include/facebook.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'fb-login/vendor/autoload.php';
+$fb = new Facebook\Facebook([
+    'app_id' => '553606428650853',
+    'app_secret' => '69b04fa4f0237eb2257bd8d2aa517d52',
+    'default_graph_version' => 'v4.0'
+]);
